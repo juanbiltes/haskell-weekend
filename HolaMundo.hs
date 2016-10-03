ce848bfd62c39dc991309dde70105397f9aaa748
Juan <juan.biltes@gmail.com>
2016-10-03 13:10:53 -0300

    first commit

---
+++ HolaMundo.hs
@@ -0,0 +1,8 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+  then x
+  else x*2
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+vectorialProduct xs xz = [ x*y | x <- xs, y <- xz]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
