a4cc88eaf8eed3f1fc920604d770b0c4d6636183
Juan Ignacio Ostroski <juan.biltes@gmail.com>
2016-10-03 23:22:53 -0300

    Hasta Modulos

--- HolaMundo.hs
+++ HolaMundo.hs
@@ -3,6 +3,30 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
   then x
   else x*2
+
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
 vectorialProduct xs xz = [ x*y | x <- xs, y <- xz]
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+capital :: String -> String
+capital "" = "¡Una cadena vacía!"
+capital all@(x:_) = "La primera letra de " ++ all ++ " es " ++ [x]
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in  sideArea + 2 * topArea
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
